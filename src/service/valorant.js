24d0b9de47047ed7fad3a6c03ee10ab4c5277944
Felipe Marques <fmarques.ds@gmail.com>
2025-04-12 18:04:33 -0300

    Update valorantService

--- src/service/valorant.js
+++ src/service/valorant.js
@@ -11,17 +11,17 @@ export class ValorantService {
     _buildMMR(puuid, fetchMMRResponseBody) {
         return new MMR(
             puuid,
-            fetchMMRResponseBody.name + '#' + fetchMMRResponseBody.tag,
-            fetchMMRResponseBody.data[0].match_id,
-            fetchMMRResponseBody.data[0].map.name,
-            fetchMMRResponseBody.data[0].last_mmr_change,
-            fetchMMRResponseBody.data[0].tier.name,
-            fetchMMRResponseBody.data[0].ranking_in_tier,
+            fetchMMRResponseBody.data.account.name + '#' + fetchMMRResponseBody.data.account.tag,
+            fetchMMRResponseBody.data.history[0].match_id,
+            fetchMMRResponseBody.data.history[0].map.name,
+            fetchMMRResponseBody.data.history[0].last_change,
+            fetchMMRResponseBody.data.history[0].tier.name,
+            fetchMMRResponseBody.data.history[0].rr,
         );
     }
 
     fetchMMR(puuid, lastMatchId, callback) {
-        return fetch(`https://api.henrikdev.xyz/valorant/v1/by-puuid/lifetime/mmr-history/br/${puuid}?size=1`, {headers: {'Authorization': this._token}})
+        return fetch(`https://api.henrikdev.xyz/valorant/v2/by-puuid/mmr-history/br/pc/${puuid}?size=1`, {headers: {'Authorization': this._token}})
             .then(res => res.json())
             .then(responseBody => {
                 const mmr = this._buildMMR(puuid, responseBody);
